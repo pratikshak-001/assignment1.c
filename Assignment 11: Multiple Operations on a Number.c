be8df58da54d081cede7cda0c6e417ec7faf68ca
pratikshak-001 <pratikshakvibes@gmail.com>
2026-04-02 18:44:32 +0530

    Add basic structure for multiple operations on a number

---
+++ Assignment 11: Multiple Operations on a Number.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+#include <math.h>
+
+int main(){
+  int num,i, choice;
+  int fact =1, isPrime =1;
+printf("Enter number:");
+scanf("%d", &num);
+
+printf("Enter number:");
+scanf("%d", &num);
+printf("1.Square\n2.Cube\n3.Square Root\n4.Prime Check\n5.Factorial\n6.Prime Factors\n");
+printf("Enter choice:");
+scanf("%d",&choice);
+
+switch(choice)
